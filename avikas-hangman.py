1218794b0aa4ff0e84206bb176eb0cf852eb0628
adithyakini <67313225+adithyakini@users.noreply.github.com>
2025-07-29 13:12:32 +0530

    d

--- avikas-hangman.py
+++ avikas-hangman.py
@@ -91,7 +91,7 @@ if 'word_index' not in st.session_state:
 
 # Main game UI
 st.markdown(
-    "<h1 style='font-size: 50px; color: blue; text-align: center;'>FARTMAN Spelling Game for AVIKA</h1>", 
+    "<h1 style='font-size: 30px; color: purple; text-align: center;'>FARTMAN Spelling Game for AVIKA</h1>", 
     unsafe_allow_html=True
 )
 st.markdown(f"**Score**: {st.session_state.correct_count}/{st.session_state.total_attempted} | **Remaining Words**: {len(WORDS) - st.session_state.total_attempted}")
