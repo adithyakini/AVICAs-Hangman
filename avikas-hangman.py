3725a08b0420b7b6c83eb22752bcf17ddf022eb6
adithyakini <67313225+adithyakini@users.noreply.github.com>
2025-07-29 13:17:13 +0530

    s

--- avikas-hangman.py
+++ avikas-hangman.py
@@ -116,7 +116,7 @@ if submit and guess and guess.isalpha():
         if letter not in st.session_state.guessed_letters:
             play_sound("wrongguess.mp3")
             st.session_state.guessed_letters.append(letter)
-            st.session_state.tries -= 1
+            st.session_state.tries += 1
             st.session_state.wrong_guesses += 1
 
 st.header(' '.join(st.session_state.guessed))
@@ -134,7 +134,7 @@ show_hangman_image(st.session_state.tries)
 if '_' not in st.session_state.guessed:
     play_sound("win.mp3")
     st.success(f"🎉 YAY! You spelt '{st.session_state.word}' correctly!")
-    st.session_state.correct_count -= 1
+    st.session_state.correct_count += 1
     st.session_state.total_attempted += 1
     if st.button("Next Word"):
         st.session_state.word_index += 1
